0285a3718e563d763bcd62483a9f49d98d86dd14
Aleksey Burn <41241809+alekseyburn@users.noreply.github.com>
2018-12-14 17:25:58 +0300

    добавил анимацию и проверку полей

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ var storage = {
   login: "",
   email: ""
 };
+var overlay = document.querySelector(".overlay");
 
 try {
   storage.login = localStorage.getItem("login");
@@ -21,6 +22,7 @@ try {
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+  overlay.classList.add("overlay-show");
 
   if (storage.login) {
     login.value = storage.login;
@@ -40,12 +42,16 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  overlay.classList.remove("overlay-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
   if (!login.value || !email.value || !textarea.value) {
     evt.preventDefault();
-    console.log("error");
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
       localStorage.setItem("login", login.value);
@@ -59,6 +65,8 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
+      overlay.classList.remove("overlay-show");
+      popup.classList.remove("modal-error");
     }
   }
 })
